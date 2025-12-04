40bea0478edae52700a4b8804549b238ea5aad0b
Lachie King <kingy00112233@gmail.com>
2025-12-04 03:03:45 +0000

    add documents for task 2. adding a node to the right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right child of another node
+ * @parent: pointer to the parent of the new node
+ * @value: value of the new node
+ * Return: address of new node or NULL on fail, or if parent is NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+	binary_tree_t *temp;
+
+	/* if parent is empty, return NULL */
+	if (parent == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	
+	/* set the value for the node and assign it's parent as the given parent */
+	node->n = value;
+	node->parent = parent;
+
+	/* if a node already exists in parent->right,
+	 * asign temp to the old node
+	 * asign temp to the right child of the new node
+	 * make the new node the parent of the old node */
+	if (parent->right)
+	{
+		temp = parent->right;
+		node->right = temp;
+		temp->parent = node;
+	}
+	/* if it doesn't exist, asign the right child to NULL */
+	else
+		node->right = NULL;
+
+	/* asign the left child of the node to NULL
+	 * asign the right child of the parent to the new node */
+	node->left = NULL;
+	parent->right = node;
+}
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This README outlines what each function does in regards to binary trees
 
 1-binary_tree_insert_left      - inserts a node as the left child of another node. if one already exists, it will replace that node and the old node will become the left-child of the new node
 
+2-binary_tree_insert_right     - inserts a node as the right child of another node. If one already exists, it will replace thta node and the old node will become the right-child of the new node
+
