0f17c76b812a824bf3d7112fae038d6943fc6e49
Lachie King <kingy00112233@gmail.com>
2025-12-09 02:16:20 +0000

    task 17

--- binary_trees.h
+++ binary_trees.h
@@ -44,5 +44,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_is_perfect_rec(const binary_tree_t *tree, size_t depth, size_t level);
 size_t binary_tree_depth(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif
