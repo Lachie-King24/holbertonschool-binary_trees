d28785d7881e2d7925b13867d99890360ccd9503
Lachie King <kingy00112233@gmail.com>
2025-12-09 01:21:16 +0000

    change

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,14 +17,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
 
-	right = binary_tree_height(tree->right);
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
 
-	if (left > right)
-		return (left);
-	else
-		return (right);
+	return (left > right ? left : right);
 }
 
 /**
