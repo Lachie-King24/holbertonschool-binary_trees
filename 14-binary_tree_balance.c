17b8c939776206684c9578314bb1dd638c0d303b
Lachie King <kingy00112233@gmail.com>
2025-12-09 01:06:05 +0000

    please work

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -47,10 +47,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL)
-		left_height = (int)binary_tree_height(tree->left);
+		left_height = 1 + binary_tree_height(tree->left);
 
 	if (tree->right != NULL) 
-		right_height = (int)binary_tree_height(tree->right);
+		right_height = 1 + binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height);
